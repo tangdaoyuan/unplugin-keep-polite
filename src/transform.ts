a215931747c51cf6d9d78344a834f896decb2aec
tangdaoyuan <1197633750@qq.com>
2022-04-23 18:59:34 +0800

    feat: support logger

--- src/transform.ts
+++ src/transform.ts
@@ -1,10 +1,20 @@
-/* eslint-disable no-console */
 import Words from 'impolite-word'
 import MagicString from 'magic-string'
 import type { TransformResult } from 'unplugin'
+import { bold, cyan, dim, italic, red } from 'picocolors'
 import type { UserOptions } from './types'
 
 const impoliteSet = new Set(Words)
+
+function logOutput(word: string, column: number, filePath: string) {
+  const _word = red(bold(word))
+  const _filePath = cyan(italic(filePath))
+  const _date = dim(new Date().toLocaleTimeString())
+  // eslint-disable-next-line no-console
+  console.log(
+    `${_date} ${cyan(bold('[vite:polite]'))} ${red('impolite words detected:')} ${_word} in ${_filePath} at line${cyan(column)}`,
+  )
+}
 
 export function transform(code: string, id: string, options: UserOptions): TransformResult {
   const _code = new MagicString(code)
@@ -13,8 +23,7 @@ export function transform(code: string, id: string, options: UserOptions): Trans
   lines.forEach((line, ind) => {
     impoliteSet.forEach((word) => {
       if (new RegExp(word, 'g').test(line)) {
-        // logger
-        console.log(`Don't use impolite word: '${word}' in ${id} at line ${ind + 1}`)
+        logOutput(word, ind + 1, id)
         // TODO
         // support replace regex
         if (options.auto) {
