561d45f4c36da95ab70e885394c11d2410895940
tangdaoyuan <1197633750@qq.com>
2022-04-26 21:50:55 +0800

    style: some verbose

--- src/transform.ts
+++ src/transform.ts
@@ -12,7 +12,7 @@ function logOutput(word: string, column: number, filePath: string) {
   const _date = dim(new Date().toLocaleTimeString())
   // eslint-disable-next-line no-console
   console.log(
-    `${_date} ${cyan(bold('[vite:polite]'))} ${red('impolite words detected:')} ${_word} in ${_filePath} at line${cyan(column)}`,
+    `${_date} ${cyan(bold('[vite:polite]'))} ${red('impolite words detected:')} ${_word} in ${_filePath} at line ${cyan(column)}`,
   )
 }
 
