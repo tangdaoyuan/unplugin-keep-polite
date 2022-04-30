84a90ff125d78e1e778dcb5380765304d8a9ada4
tangdaoyuan <1197633750@qq.com>
2022-05-01 02:06:35 +0800

    chore: playground support plugin reload

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,13 +1,19 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { vitePlugin } from '../src/index'
+import VitePluginReload from 'vite-plugin-reload'
+import { vitePlugin as VitePluginPolite } from '../src/index'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vitePlugin({
-    auto: false,
-  })],
+  plugins: [
+    vue(),
+    VitePluginReload({
+      includes: ['../src/**/*.{ts,tsx}'],
+    }),
+    VitePluginPolite({
+      auto: false,
+    })],
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
